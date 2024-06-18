f589eeab664020fd11330ce519de13823d30be18
js <2kpppk@gmail.com>
2024-06-18 19:58:45 +0900

    첫 커밋

---
+++ src/main/java/com/gpt_hub/GptHubApplication.java
@@ -0,0 +1,13 @@
+package com.gpt_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GptHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GptHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gpt_hub/GptHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpt_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GptHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
