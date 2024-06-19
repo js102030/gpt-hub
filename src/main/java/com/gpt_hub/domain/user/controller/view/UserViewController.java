1cfa29360ac2f0e808948b0d91f708ca0dd4733c
js <2kpppk@gmail.com>
2024-06-19 22:16:16 +0900

    초기 UserViewController 작성

---
+++ src/main/java/com/gpt_hub/domain/user/controller/view/UserViewController.java
@@ -0,0 +1,13 @@
+package com.gpt_hub.domain.user.controller.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserViewController {
+
+    @GetMapping("/signup")
+    public String join() {
+        return "html/signup";
+    }
+}
