17301725aad110b76360fafb980d938301f855ba
js <2kpppk@gmail.com>
2024-06-19 22:18:17 +0900

    초기 UserRepository 작성

---
+++ src/main/java/com/gpt_hub/domain/user/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.gpt_hub.domain.user.repository;
+
+import com.gpt_hub.domain.user.entity.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByLoginId(String loginId);
+
+    boolean existsByNickname(String nickname);
+
+    boolean existsByEmail(String email);
+
+    Optional<User> findByLoginId(String loginId);
+}
