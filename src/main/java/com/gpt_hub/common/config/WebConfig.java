edfb06c6051d39f9db635c19802cdc1547eb4a16
js <2kpppk@gmail.com>
2024-06-29 11:24:03 +0900

    아규먼트 리졸버 등록 코드 작성

---
+++ src/main/java/com/gpt_hub/common/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.gpt_hub.common.config;
+
+import com.gpt_hub.domain.user.annotation.LoginUserIdArgumentResolver;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final LoginUserIdArgumentResolver loginUserIdArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginUserIdArgumentResolver);
+    }
+}
