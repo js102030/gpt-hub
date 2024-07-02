7f622e95b30a9f7529bb652bf309714a71839d63
js <2kpppk@gmail.com>
2024-07-02 13:29:00 +0900

    api url 변경, 응답데이터 변경, 로그인필수로 수정

--- src/main/java/com/gpt_hub/domain/gptdata/controller/GptController.java
+++ src/main/java/com/gpt_hub/domain/gptdata/controller/GptController.java
@@ -3,8 +3,9 @@ package com.gpt_hub.domain.gptdata.controller;
 import static org.springframework.http.HttpStatus.NO_CONTENT;
 import static org.springframework.http.HttpStatus.OK;
 
+import com.gpt_hub.common.annotation.LoginUserId;
+import com.gpt_hub.domain.gptdata.dto.GptDataResponse;
 import com.gpt_hub.domain.gptdata.dto.GptQuestion;
-import com.gpt_hub.domain.gptdata.dto.GptResponse;
 import com.gpt_hub.domain.gptdata.service.GptDataService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -23,13 +24,13 @@ public class GptController {
 
     private final GptDataService gptDataService;
 
-    @PostMapping("/ask-gpt")
+    @PostMapping("/gpt")
     @ResponseStatus(OK)
-    public GptResponse askGpt(@RequestBody @Valid GptQuestion gptQuestion) {
-        return gptDataService.askGpt(gptQuestion);
+    public GptDataResponse askGpt(@LoginUserId Long loginUserId, @RequestBody @Valid GptQuestion gptQuestion) {
+        return gptDataService.askGpt(loginUserId, gptQuestion);
     }
 
-    @DeleteMapping("/ask-gpt/{gptDataId}")
+    @DeleteMapping("/gpt/{gptDataId}")
     @ResponseStatus(NO_CONTENT)
     public void deleteGptData(@PathVariable Long gptDataId) {
         gptDataService.deleteGptData(gptDataId);
